4abefe4d23c1841d43412cf1c5d23bdaf3036465
MoiraVH <moiravh1102@gmail.com>
2025-10-29 17:49:08 -0600

    Limpio inicial :d

---
+++ ivonne-banana/src/main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+function createWindow() {
+  const win = new BrowserWindow({ width: 900, height: 680 });
+  win.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  );
+  win.webContents.openDevTools();
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
